a8bbf8a9b430727a896f759436ebce17ad393993
Beatriz <157439476+Iam-BeatrizM@users.noreply.github.com>
2025-04-29 05:31:06 -0300

    TCC
    FLUXOTECH

--- php/editar_usuario.php
+++ php/editar_usuario.php
@@ -22,14 +22,15 @@ $usuario = mysqli_fetch_assoc($res);
 <head>
     <title>Editar Usuário</title>
     <meta charset="utf-8">
-    <link rel="shortcut icon" href="../img/logo.png" type="image/x-icon">
+    <link rel="shortcut icon" href="../img/logocentro.png" type="image/x-icon">
     <link rel="stylesheet" href="../style/geral.css">
     <link rel="stylesheet" href="../style/editaruser.css">
 </head>
 <body>
     <header>
         <nav>
-            <a href="../inicio.html">Início</a>
+            <a href="#"><img src="../img/logocentropreta.png" alt="logo" height="30%" width="28%"></a>
+            <a href="../index.html">Início</a>
             <a href="../pages/suporte.html">Suporte</a>
             <a href="../php/valida_login_adm.php?bt=sair"><button type="submit" class="btn"><img src="../img/logout.svg" alt=""></button></a>
         </nav>
@@ -54,5 +55,11 @@ $usuario = mysqli_fetch_assoc($res);
             </form>
         </div>
     </main>
+
+    <footer>
+        <p> # Entre em contato. <br> Telefone: (61) 93333-2254 <br> Endereço Quadra 123A Rua Inês - Vale do Paraíso, DF CEP 76923-000 <br> # Copyright @2024 FluxoTech. All rights reserved. </p>
+        <img class="pe" src="../img/logocentropreta.png" alt="logocentropreta" height="150px">
+    </footer>
+
 </body>
 </html>
--- php/usuarios.php
+++ php/usuarios.php
@@ -6,14 +6,15 @@
     <meta charset="utf-8">
     <link rel="stylesheet" href="../style/geral.css">
     <link rel="stylesheet" href="../style/user.css">
-    <link rel="shortcut icon" href="../img/logo.png" type="image/x-icon">
+    <link rel="shortcut icon" href="../img/logocentro.png" type="image/x-icon">
 
 </head>
 
 <body>
     <header>
         <nav>
-            <a href="../inicio.html">Início</a>
+            <a href="#"><img src="../img/logocentropreta.png" alt="logo" height="30%" width="28%"></a>
+            <a href="../index.html">Início</a>
             <a href="../pages/cadastro_user.html">Cadastrar</a>
             <a href="../pages/suporte.html">Suporte</a>
             <a href="../php/valida_login_adm.php?bt=sair"><button type="submit" class="btn">Sair</button></a>
@@ -62,7 +63,10 @@
         }
     </script>
 
-    <footer></footer>
+    <footer>
+        <p> # Entre em contato. <br> Telefone: (61) 93333-2254 <br> Endereço Quadra 123A Rua Inês - Vale do Paraíso, DF CEP 76923-000 <br> # Copyright @2024 FluxoTech. All rights reserved. </p>
+        <img class="pe" src="../img/logocentropreta.png" alt="logocentropreta" height="150px">
+    </footer>
 </body>
 
 </html>

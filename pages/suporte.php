ca39dbdc7561f1317ecbccb13ff53fbf88939ab4
Beatriz <157439476+Iam-BeatrizM@users.noreply.github.com>
2025-06-15 18:50:33 -0300

    TCC
    ÚLTIMO COMMITTTTTTT!!!!

--- pages/suporte.php
+++ pages/suporte.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <title>Suporte</title>
@@ -7,47 +8,51 @@
     <link rel="shortcut icon" href="../img/logocentro.png" type="image/x-icon">
     <link rel="stylesheet" href="../style/suporte.css">
 </head>
+
 <body>
     <header>
-        <nav> 
+        <nav>
             <div class="nesquerda">
                 <a href="#"><img src="../img/LogoSite.png" alt="logo"></a>
             </div>
-            
-                <div class="ndireita">
-                    <?php
-                session_start(); 
-                    if (isset($_SESSION['autenticado']) && $_SESSION['autenticado'] === 'SIM') {
-                        echo "<p>Logado</p>";
-                    } else {
-                        echo "<p>Você não está logado.</p>";
-                    }
-            ?>
-            <button class="btn" onclick="voltar()">Voltar</button>
-                </div>
+
+            <div class="ndireita">
+                <?php
+                session_start();
+                if (isset($_SESSION['autenticado']) && $_SESSION['autenticado'] === 'SIM') {
+                    echo "<p>Logado</p>";
+                } else {
+                    echo "<p>Você não está logado.</p>";
+                }
+                ?>
+                <button class="btn" onclick="voltar()">Voltar</button>
+            </div>
 
             <script>
-                function voltar() { window.history.back(); }
+                function voltar() {
+                    window.history.back();
+                }
             </script>
         </nav>
     </header>
     <main>
-        <div class="card-header">
-            <h2>Entre em contato com FluxoTech</h2>
-        </div>
         <div class="container">
-    <form action="../php/env_suporte.php" method="post">
-        <label>Seu nome:</label><br>
-        <input class="conteudo" type="text" name="nome" required /><br>
+            <div class="card-header">
+                <h2>Entre em contato</h2>
+            </div>
+            <form action="../php/env_suporte.php" method="post">
+                <label>Seu nome:</label><br>
+                <input class="conteudo" type="text" name="nome" required /><br>
 
-        <label>Seu email:</label><br>
-        <input class="conteudo" type="email" name="email" required /><br>
+                <label>Seu email:</label><br>
+                <input class="conteudo" type="email" name="email" required /><br>
 
 
-        <label>Mensagem:</label><br>
-        <textarea name="mensagem" rows="6" required></textarea><br>
+                <label>Mensagem:</label><br>
+                <textarea name="mensagem" rows="6" required></textarea><br>
 
-        <button class="btn" type="submit">Enviar</button>
+                <button class="btn" type="submit">Enviar</button>
+            </form>
         </div>
     </main>
 </body>
@@ -57,7 +62,7 @@
     <p> # Entre em contato. <br> Telefone: (61) 93333-2254 <br> E-mail: fluxotechsystems@gmail.com <br> Endereço
         Quadra 123A Rua Inês - Vale do Paraíso, DF CEP 76923-000 <br> # Copyright @2024 FluxoTech. All rights
         reserved. </p>
-        <img class="pe" src="../img/LogoSite.png" alt="LogoSite" height="34px">
+    <img class="pe" src="../img/LogoSite.png" alt="LogoSite" height="34px">
 </footer>
-</html>
 
+</html>
